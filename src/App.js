e212de7e782597de94094dee5a1ee8c678b236f6
Aman gupta <amangupta.95@outlook.com>
2021-06-04 21:17:12 +0530

    added search-box component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './components/header/header.component';
+import SearchBox from './components/search-box/search-box.component';
 
 import './App.scss';
 
@@ -14,6 +15,7 @@ class App extends React.Component {
 		return (
 			<div className="App">
 				<Header />
+				<SearchBox placeholder={`Search for a country...`} />
 			</div>
 		);
 	}
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './search-box.style.scss';
+
+const SearchBox = ({ placeholder, handleChange }) => (
+	<div className="search-container">
+		<button className="search-icon-btn">
+			<i class="fas fa-search search-icon"></i>
+		</button>
+		<input
+			className="search"
+			type="search"
+			placeholder={placeholder}
+			onChange={handleChange}
+		/>
+	</div>
+);
+
+export default SearchBox;
