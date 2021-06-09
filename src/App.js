ad08371ee20349a59d3dec04e6560cf6309fbf37
Aman gupta <amangupta.95@outlook.com>
2021-06-09 21:05:10 +0530

    added rounting

--- src/App.js
+++ src/App.js
@@ -1,64 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/header/header.component';
-import SearchBox from './components/search-box/search-box.component';
-import Dropdown from './components/dropdown/dropdown.component';
-import CardList from './components/card-list/card-list.component';
+
+import HomePage from './pages/homepage/homepage.component';
+import Details  from './pages/detail/detail.component';
 
 import './App.scss';
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			countries: [],
-			region: 'all',
-			searchField: '',
-		};
-	}
-
-	componentDidMount() {
-		fetch('https://restcountries.eu/rest/v2/all')
-			.then((res) => res.json())
-			.then((data) => this.setState({ countries: data }));
-	}
-
-	handleRegion = (e) => this.setState({ region: e.target.value });
-	handleSearch = (e) => this.setState({ searchField: e.target.value });
-
-	render() {
-		const { countries, region, searchField } = this.state;
-		const regions = countries
-			.map((e) => e.region)
-			.filter((e, index, self) => self.indexOf(e) === index && e !== '');
-		let filteredRegion = countries;
-		if (region !== 'all')
-			filteredRegion = countries.filter((country) =>
-				country.region.toLowerCase().includes(region.toLowerCase())
-			);
-		const filterCountries = filteredRegion.filter((country) =>
-			country.name.toLowerCase().includes(searchField.toLowerCase())
-		);
-		return (
-			<div className="App">
-				<Header />
-				<div className="container">
-					<div className="controllers">
-						<SearchBox
-							placeholder={`Search for a country...`}
-							handleChange={this.handleSearch}
-						/>
-						<Dropdown
-							regions={regions}
-							title="Filter by region"
-							handleChange={this.handleRegion}
-						/>
-					</div>
-					<CardList countries={filterCountries} />
-				</div>
-			</div>
-		);
-	}
-}
+const App = () => (
+	<Router>
+		<Header />
+		<Switch>
+			<Route exact path="/" component={HomePage} />
+			<Route path="/:name" component={Details}/>
+		</Switch>
+	</Router>
+);
 
 export default App;
