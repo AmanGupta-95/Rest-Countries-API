75b9c3a0d6772a61dc2d77d59a124d2ead07a2af
Aman gupta <amangupta.95@outlook.com>
2021-06-13 11:26:34 +0530

    added link

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './card.style.scss';
 
 const Card = ({ country }) => {
 	return (
-		<div className="card-container">
-			<img className="card-flag" alt="flag" src={country.flag} />
-			<div className="card-description">
-				<h3 className="card-title">{country.name}</h3>
-				<div className="card-text">
-					<span>Population: </span>
-					{country.population.toLocaleString()}
-				</div>
-				<div className="card-text">
-					<span>Region: </span>
-					{country.region}
-				</div>
-				<div className="card-text">
-					<span>Capital: </span>
-					{country.capital}
+		<Link to={`/${country.name}`} className="link">
+			<div className="card-container">
+				<img className="card-flag" alt="flag" src={country.flag} />
+				<div className="card-description">
+					<h3 className="card-title">{country.name}</h3>
+					<div className="card-text">
+						<span>Population: </span>
+						{country.population.toLocaleString()}
+					</div>
+					<div className="card-text">
+						<span>Region: </span>
+						{country.region}
+					</div>
+					<div className="card-text">
+						<span>Capital: </span>
+						{country.capital}
+					</div>
 				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
